35aa498d74bc8a859fddfd37dc1cb1d61f9d20ec
itiguila-2024295 <itiguila-2024295@kinal.edu.gt>
2026-01-20 23:12:09 -0600

    feat: implementa navegación entre escenas y vista de agregar contacto

--- Scripts/agenda.js
+++ Scripts/agenda.js
@@ -8,6 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var userName = document.getElementById('userName');
     var userEmail = document.getElementById('userEmail');
 
+    const navItems = document.querySelectorAll('.nav-item');
+    const views = document.querySelectorAll('.view');
+
+    const contactForm = document.getElementById('contactForm');
+    const successModal = document.getElementById('modalExito');
+    const successOkBtn = document.getElementById('botonModal');
+
     if (userButton && userDropdown) {
         userButton.addEventListener('click', function (e) {
             e.stopPropagation();
@@ -40,4 +47,56 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location.href = '../index.html';
         });
     }
+
+    // Camabio de vistas
+
+    function changeView(viewName) {
+        
+        views.forEach(view => view.classList.remove('active'));
+        document.getElementById(`${viewName}-view`)?.classList.add('active');
+
+        
+        navItems.forEach(btn => btn.classList.remove('active'));
+        document.querySelector(`[data-view="${viewName}"]`)?.classList.add('active');
+    }
+
+    
+    navItems.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const view = btn.dataset.view;
+            changeView(view);
+        });
+    });
+
+
+    // Abrir y cerrar el Modal de éxito
+
+    function openSuccessModal() {
+        if (successModal) {
+            successModal.classList.add('active');
+            successModal.setAttribute('aria-hidden', 'false');
+        }
+    }
+
+    function closeSuccessModal() {
+        if (successModal) {
+            successModal.classList.remove('active');
+            successModal.setAttribute('aria-hidden', 'true');
+        }
+    }
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            openSuccessModal();
+            contactForm.reset();
+        });
+    }
+
+    if (successOkBtn) {
+        successOkBtn.addEventListener('click', function () {
+            closeSuccessModal();
+        });
+    }
 });
+
